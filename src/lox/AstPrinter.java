0cf3175a28e6eb22067dd148a74d99ac623d9101
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-04-02 00:31:38 +0530

    updated to support ternary (still not really actually complete)

--- src/lox/AstPrinter.java
+++ src/lox/AstPrinter.java
@@ -4,6 +4,9 @@ class AstPrinter implements Expr.Visitor<String> {
     String print(Expr expr) {
         return expr.accept(this);
     }
+    public String visitTernaryExpr(Expr.Ternary expr) {
+        return parenthesize("ternary", expr.first, expr.middle, expr.last);
+    }
     @Override
     public String visitBinaryExpr(Expr.Binary expr) {
         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
